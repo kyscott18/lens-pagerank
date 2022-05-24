cfed210768f73d5f4bcb3aa23949d1aa07dfa603
Kyle Scott <kyscott@umich.edu>
2022-05-23 20:43:46 -0400

    remove packages

--- src/scripts/fetch.ts
+++ src/scripts/fetch.ts
@@ -1,9 +1,9 @@
-import { BigNumber } from "ethers";
-
 import { getFollowing, getProfiles, getTotalProfiles } from "../apollo";
+import { numToId } from "./utils";
 
 export const pagerank = async (): Promise<void> => {
-  const testProfile = BigNumber.from(16).toHexString();
+  const testProfile = numToId(16);
+  console.log(testProfile);
   const testAddress = "0x88a769db5055B046c9A45Db621978bbEC65c8c5b";
 
   const totalProfilesData = await getTotalProfiles();
---
+++ src/scripts/utils.ts
@@ -0,0 +1,1 @@
+export const numToId = (num: number): string => "0x" + num.toString(16);
